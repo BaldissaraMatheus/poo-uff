c4c7e5f3089df84fc25632f538b4597cd3bb60e7
BaldissaraMatheus <baldissaramatheus@gmail.com>
2018-09-30 10:39:49 -0300

    Adicionado aula5Ex1

---
+++ aula5/aula5Ex1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class aula5Ex1 {
+  public static void listaPessoas(int len) {
+    String nomes[] = new String[len];
+    int idades[] = new int[len];
+
+    Scanner scStr = new Scanner(System.in);
+    Scanner scInt = new Scanner(System.in);
+    
+    for (int i=0; i<len; i++) {
+      System.out.println("Digite o nome " + i);
+      nomes[i] = scStr.nextLine();
+      System.out.println("Digite a idade " + i);
+      idades[i] = scInt.nextInt();
+    }
+
+    scStr.close();
+    scInt.close();
+
+    for (int i=0; i<len; i++) {
+      for (int j=i+1; j<len; j++) {
+        if (nomes[i].compareTo(nomes[j])>0) {
+          String tempStr = nomes[i];
+          nomes[i] = nomes[j];
+          nomes[j] = tempStr;
+
+          int tempInt = idades[i];
+          idades[i] = idades[j];
+          idades[j] = tempInt;
+        }
+      }
+    }
+
+    for (int i=0; i<len; i++) {
+      System.out.println(nomes[i] +" tem " + idades[i] + " anos!");
+    }
+  }
+
+  public static void main(String[] args) {
+    listaPessoas(5);
+  }
+}
