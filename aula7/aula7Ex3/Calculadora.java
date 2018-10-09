c3d02d370ebe8da52048c3d23a3a7d3f66a1530c
BaldissaraMatheus <baldissaramatheus@gmail.com>
2018-10-08 22:31:20 -0300

    Adicionado aula7Ex3

---
+++ aula7/aula7Ex3/Calculadora.java
@@ -0,0 +1,38 @@
+package aula7ex3package;
+
+public class Calculadora {
+  private int memoria;
+
+  public Calculadora() {
+    this.memoria = 0;
+  }
+
+  public int soma(int num) {
+    this.memoria = this.memoria + num;
+    System.out.println("Memória = " + this.memoria);
+    return this.memoria;
+  }
+
+  public int subtrai(int num) {
+    this.memoria = this.memoria - num;
+    System.out.println("Memória = " + this.memoria);
+    return this.memoria;
+  }
+
+  public int multiplica(int num) {
+    this.memoria = this.memoria * num;
+    System.out.println("Memória = " + this.memoria);
+    return this.memoria;
+  }
+
+  public int divide(int num) {
+    this.memoria = this.memoria/num;
+    System.out.println("Memória = " + this.memoria);
+    return this.memoria;
+  }
+
+  public void limpaMemoria() {
+    this.memoria = 0;
+    System.out.println("Memória = " + this.memoria);
+  }
+}
