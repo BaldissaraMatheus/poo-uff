ddbfd0ce8c1f51d680ca600163ed164985985c41
BaldissaraMatheus <baldissaramatheus@gmail.com>
2018-10-14 19:37:50 -0300

    Adicionado lista1Ex5

---
+++ lista-de-exercicios-1/lista1Ex5/Radio.java
@@ -0,0 +1,9 @@
+package lista1ex1;
+
+public interface Radio {
+  public void setEmissora(String emissora, String tipoEmissora);
+  public String getEmissora();
+  public String getEmissoraTipo();
+  public void setVolumeRadio(int volume);
+  public int getVolumeRadio();
+}
---
+++ lista-de-exercicios-1/lista1Ex5/Relogio.java
@@ -0,0 +1,12 @@
+package lista1ex1;
+
+public interface Relogio {
+  public void setHorario(int horario);
+  public int getHorario();
+  public void setHorarioAlarme(int horario);
+  public int getHorarioAlarme();
+  public void setVolumeRelogio(int volume);
+  public int getVolumeRelogio();
+  public void ligaAlarme();
+  public void desligaAlarme();
+}
