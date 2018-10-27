177d85f965a97bd2228e17f2dbdd2864c7e58396
Matheus Baldissara <baldissaramatheus@gmail.com>
2018-10-27 17:36:29 -0300

    Update aula7Ex2.java

--- aula7/aula7Ex2/aula7Ex2.java
+++ aula7/aula7Ex2/aula7Ex2.java
@@ -3,7 +3,7 @@ package aula7ex2package;
 public class aula7Ex2 {
   public static void main(String[] args) {
     Pessoa p1 = new Pessoa("Orora", 22);
-    Pessoa p2 = new Pessoa("Fausto", 68);
+    Pessoa p2 = new Pessoa("Faustao", 68);
     Pessoa p3 = new Pessoa("Laninha", 33);
 
     Agenda agenda = new Agenda();
