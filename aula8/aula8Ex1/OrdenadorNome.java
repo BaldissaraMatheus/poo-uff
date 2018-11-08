baaec3ab7549dd15b2bf18d9bd2fbda02332a40f
Matheus Baldissara <baldissaramatheus@gmail.com>
2018-11-08 15:52:47 -0200

    Update OrdenadorNome.java

--- aula8/aula8Ex1/OrdenadorNome.java
+++ aula8/aula8Ex1/OrdenadorNome.java
@@ -2,21 +2,4 @@ package aula8ex1package;
 
 public class OrdenadorNome extends Ordenador{
 
-  public Agenda ordena(Agenda agenda) {
-    int len = agenda.getLen();
-    Pessoa pessoas[] = agenda.getPessoas();
-
-    for (int i=0; i<len; i++) {
-      for (int j=i+1; j<len; j++) {
-        if ((pessoas[i].getNome()).compareTo(pessoas[j].getNome())>0) {
-          Pessoa temp = pessoas[i];
-          pessoas[i] =  pessoas[j];
-          pessoas[j] = temp;
-        }
-      }
-    }
-
-    agenda.setPessoas(pessoas);
-    return agenda;
-  };
 }
