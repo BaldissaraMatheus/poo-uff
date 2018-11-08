755971c289ad2629427d1cd04a53d5ee0852a052
Matheus Baldissara <baldissaramatheus@gmail.com>
2018-11-08 15:52:07 -0200

    Update Ordenador.java

--- aula8/aula8Ex1/Ordenador.java
+++ aula8/aula8Ex1/Ordenador.java
@@ -2,5 +2,21 @@ package aula8ex1package;
 
 public abstract class Ordenador {
 
-  protected abstract Agenda ordena(Agenda agenda);
+  protected abstract Agenda ordena(Agenda agenda) {
+    int len = agenda.getLen();
+    Pessoa pessoas[] = agenda.getPessoas();
+
+    for (int i=0; i<len; i++) {
+      for (int j=i+1; j<len; j++) {
+        if (pessoas[i].getIdade() > pessoas[j].getIdade()) {
+          Pessoa temp = pessoas[i];
+          pessoas[i] =  pessoas[j];
+          pessoas[j] = temp;
+        }
+      }
+    }
+
+    agenda.setPessoas(pessoas);
+    return agenda;
+  }
 }
