18e63cd3f2e7a21c577817f69773eff0b0672e54
Matheus Baldissara <baldissaramatheus@gmail.com>
2019-07-17 19:35:25 -0300

    Aprimora resolução de exercícios

--- aula5/exercicio1.java
+++ aula5/exercicio1.java
@@ -3,45 +3,94 @@ import java.util.Scanner;
 
 public class exercicio1 {
 
-  public static void listaPessoas(int len) {
-    String nomes[] = new String[len];
-    int idades[] = new int[len];
-
-    Scanner scStr = new Scanner(System.in);
-    Scanner scInt = new Scanner(System.in);
+	static Scanner scanner;
+	
+	public static void leEListaPessoas(int len) {
+	    String nomes[] = new String[len];
+	    int idades[] = new int[len];
+	    
+	    for (int i = 0; i < len; i++) {
+	      nomes[i] = leNome(i);
+	      idades[i] = leIdade(nomes[i]);
+	    }
+	    
+	    listaPorOrdemAlfabetica(nomes, idades);
+	    listaPorOrdemDeIdade(nomes, idades);
     
-    for (int i=0; i<len; i++) {
-      System.out.println("Digite o nome " + (i+1));
-      nomes[i] = scStr.nextLine();
-      System.out.println("Digite a idade " + (i+1));
-      idades[i] = scInt.nextInt();
-    }
+	}
 
-    scStr.close();
-    scInt.close();
-
-    for (int i=0; i<len; i++) {
-      for (int j=i+1; j<len; j++) {
-        if (nomes[i].compareTo(nomes[j])>0) {
-          String tempStr = nomes[i];
-          nomes[i] = nomes[j];
-          nomes[j] = tempStr;
-
-          int tempInt = idades[i];
-          idades[i] = idades[j];
-          idades[j] = tempInt;
-        }
-      }
-    }
-
-    for (int i=0; i<len; i++) {
-      System.out.println(nomes[i] +" tem " + idades[i] + " anos!");
-    }
-  }
-
-  public static void main(String[] args) {
-    int tamanho = 20;
-
-    listaPessoas(tamanho);
-  }
+	public static String leNome(int posicao) {
+		scanner = new Scanner(System.in);
+		String nome;
+		
+		System.out.println("Digite o nome " + (posicao+1));
+		nome = scanner.nextLine();
+		
+		return nome;
+	}
+	
+	public static int leIdade(String nome) {
+		scanner = new Scanner(System.in);
+		int idade;
+		
+		System.out.println("Digite a idade de " + nome);
+		idade = scanner.nextInt();
+		
+		return idade;
+	}
+	
+	public static void listaPorOrdemAlfabetica(String[] nomes, int[] idades) {
+		System.out.println("Listando por ordem alfabética:");
+	
+	    for (int i = 0; i < nomes.length; i++) {
+	    	for (int j = i + 1; j < nomes.length; j++) {
+	    		if (nomes[i].compareTo(nomes[j]) > 0) {
+	    			swap(nomes, i, j);
+	    			swap(idades, i, j);
+	    		}
+	        }
+	    }
+    
+	    lista(nomes, idades);
+	}
+  
+	public static void listaPorOrdemDeIdade(String[] nomes, int[] idades) {
+		System.out.println("Listando por ordem crescente de idade:");
+	
+	    for (int i = 0; i < nomes.length; i++) {
+	    	for (int j = i + 1; j < nomes.length; j++) {
+	    		if (idades[j] > idades[i]) {
+	    			swap(nomes, i, j);
+	    			swap(idades, i, j);
+	    		}
+	        }
+	    }
+    
+    	lista(nomes, idades);
+	}
+  
+	public static void lista(String[] nomes, int[] idades) {
+	  for (int i = 0; i < nomes.length; i++) {
+		  System.out.println(nomes[i] +" tem " + idades[i] + " anos!");
+	  }
+	  System.out.println();
+	}
+  
+	public static void swap(String[] vetorDeNomes, int posicao1, int posicao2) {
+	  String temp = vetorDeNomes[posicao1];
+      vetorDeNomes[posicao1] = vetorDeNomes[posicao2];
+      vetorDeNomes[posicao1] = temp;
+	}
+  
+	public static void swap(int[] vetorDeIdades, int posicao1, int posicao2) {
+	  int temp = vetorDeIdades[posicao1];
+      vetorDeIdades[posicao1] = vetorDeIdades[posicao2];
+      vetorDeIdades[posicao1] = temp;
+	}
+  
+	public static void main(String[] args) {
+	    int numeroDePessoas = 10;
+	    leEListaPessoas(numeroDePessoas);
+    
+	}
 }
--- aula5/exercicio2.java
+++ aula5/exercicio2.java
@@ -6,24 +6,24 @@ public class exercicio2 {
   public static void listaNotas(int len) {
     int notas[] = new int[len];
 
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     
-    for (int i=0; i<len; i++) {
+    for (int i = 0; i < len; i++) {
       System.out.println("Digite a nota " + (i+1));
-      notas[i] = sc.nextInt();
+      notas[i] = scanner.nextInt();
     }
 
-    sc.close();
+    scanner.close();
 
     System.out.print("Notas: ");
+    
     for (int i=0; i<len; i++) {      
       System.out.print(notas[i] + " ");
     }
   }
 
   public static void main(String[] args) {
-    int tamanho = 5;
-
-    listaNotas(tamanho);
+    int numeroDeNotas = 5;
+    listaNotas(numeroDeNotas);
   }
 }
