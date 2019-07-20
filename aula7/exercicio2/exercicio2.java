91ae1a5cb2f677403e6173f01ffeadd51420c9d4
Matheus Baldissara <baldissaramatheus@gmail.com>
2019-07-20 15:54:12 -0300

    Altera nome das pessoas

--- aula7/exercicio2/exercicio2.java
+++ aula7/exercicio2/exercicio2.java
@@ -3,8 +3,8 @@ package aula7.exercicio2;
 public class exercicio2 {
   public static void main(String[] args) {
     Pessoa p1 = new Pessoa("Orora", 22);
-    Pessoa p2 = new Pessoa("Faustao", 68);
-    Pessoa p3 = new Pessoa("Laninha", 33);
+    Pessoa p2 = new Pessoa("Bilie", 17);
+    Pessoa p3 = new Pessoa("Lana", 33);
 
     Agenda agenda = new Agenda();
 
